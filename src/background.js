64c81c14f04f828cd26e96a53dc4f69c2349c529
misor.digital <misor.digital@gmail.com>
2023-04-17 12:45:31 +0300

    .eslintrc.json: + sourceType & sinleQuote; moved cons/ in src/; manifest.json: icons dir typo, author email; background.js: action onClick handler

--- src/background.js
+++ src/background.js
@@ -1,3 +1,14 @@
 const url = 'https://developer.chrome.com/docs/extensions';
 
-chrome.tabs.
+const openNewTab = (url) => {
+  return chrome.tabs.create({
+    url,
+    active: true,
+  });
+};
+
+const handleActionClick = () => {
+  openNewTab(url);
+};
+
+chrome.action.onClicked.addListener(handleActionClick);
