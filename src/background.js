5d152f133d121557c2c58e4bf71ea8c0290b9fcd
misor.digital <misor.digital@gmail.com>
2023-04-17 18:58:59 +0300

    manifest.json: + host_permissions & permissons -> alarms, tabs; background.js: separate alarm & alarmEventListener for each new tab, removed both on tab close

--- src/background.js
+++ src/background.js
@@ -1,14 +1,41 @@
 const url = 'https://developer.chrome.com/docs/extensions';
+const genericAlarmName = 'Page Manager - Reload tab id:';
+const interval = 0.05;
 
-const openNewTab = (url) => {
-  return chrome.tabs.create({
-    url,
-    active: true,
+const handleActionClick = async (url, alarmName, interval) => {
+  const tab = await chrome.tabs.create({ url, active: true });
+  const currentTabAlarmName = `${alarmName}${tab.id}`;
+  const alarmInfo = { periodInMinutes: interval };
+
+  await chrome.alarms.create(currentTabAlarmName, alarmInfo);
+
+  const alarm = await chrome.alarms.get(currentTabAlarmName);
+  const tabReloadEventListener = await chrome.alarms.onAlarm.addListener(
+    alarmEventListener.bind(alarm, tab, currentTabAlarmName)
+  );
+
+  chrome.tabs.onRemoved.addListener(async (tabId, removedInfo) => {
+    if (tabId === tab.id) {
+      await chrome.alarms.onAlarm.removeListener(tabReloadEventListener);
+      await chrome.alarms.clear(currentTabAlarmName);
+    }
   });
 };
 
-const handleActionClick = () => {
-  openNewTab(url);
+const alarmEventListener = function (tab, alarmName, alarm) {
+  if (alarm.name === alarmName) {
+    chrome.tabs.reload(tab.id);
+
+    return this;
+  }
 };
 
-chrome.action.onClicked.addListener(handleActionClick);
+const removeAlarmOnClosed = (curTabId, alarmName, tabIdWithAlarm) => {
+  if (curTabId === tabIdWithAlarm) {
+    chrome.alarms.clear(alarmName);
+  }
+};
+
+chrome.action.onClicked.addListener(
+  handleActionClick.bind(null, url, genericAlarmName, interval)
+);
